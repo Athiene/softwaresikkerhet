f1d9944065c5e954172d1cfbcbb7779e870b153a
Arvin Matthew Rapstad <101880810+Athiene@users.noreply.github.com>
2023-11-09 14:20:39 +0100

    Implemented QR Code for 2FactorAuthentication
    Implemented QR code for tor 2Factor Authentication. Require to make it pretty, but functionality wise it is good. Used a guide provided by microsoft:
    
    https://learn.microsoft.com/nb-no/aspnet/core/security/authentication/identity-enable-qrcodes?view=aspnetcore-7.0

---
+++ wwwroot/js/qr.js
@@ -0,0 +1,19 @@
+﻿window.addEventListener("load", () => {
+    const qrCodeDataElement = document.getElementById("qrCodeData");
+
+    if (qrCodeDataElement) {
+        const uri = qrCodeDataElement.getAttribute('data-url');
+
+        if (uri) {
+            new QRCode(document.getElementById("qrCode"), {
+                text: uri,
+                width: 150,
+                height: 150
+            });
+        } else {
+            console.error("QR code data URL is missing.");
+        }
+    } else {
+        console.error("QR code data element not found.");
+    }
+});
