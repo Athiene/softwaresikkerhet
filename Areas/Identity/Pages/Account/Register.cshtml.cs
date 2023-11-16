c5c62da9fdf1b4c97b07014064f127b1dbde2c5c
Arvin Matthew Rapstad <101880810+Athiene@users.noreply.github.com>
2023-11-16 15:47:55 +0100

    Fixed local login with Nickname
    There occured some migration errors since we created a user without a a Nickname.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -101,7 +101,6 @@ namespace website.Areas.Identity.Pages.Account
             
             [Required]
             [Display(Name = "Nickname")]
-            [Compare("Password", ErrorMessage = "Please enter a Nickname")]
             public string Nickname { get; set; }
         }
 
@@ -122,6 +121,7 @@ namespace website.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Nickname = Input.Nickname;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
